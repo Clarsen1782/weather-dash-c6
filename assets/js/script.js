895f6e3456436c95bece9e8816d7a6bb80d3d695
Christina Larsen <clarsen1782@outlook.com>
2023-10-10 21:29:32 -0700

    blocked by cors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 
 
 
+    
+     const apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={26fe44de11957c828f9297ab115d2423}';
+
+     const fetchDataButton = document.getElementById('button-addon2');
+     const dataContainer = document.getElementById('dataContainer');
+
+     
+     fetchDataButton.addEventListener('click', () => {
+         fetch(apiUrl)
+             .then(response => response.json())
+             .then(data => {
+                 dataContainer.innerHTML = JSON.stringify(data, null, 2);
+             })
+             .catch(error => {
+                 console.error('Error fetching data:', error);
+             });
+     });
